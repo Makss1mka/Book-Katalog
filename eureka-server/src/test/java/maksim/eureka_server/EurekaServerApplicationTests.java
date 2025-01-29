50362bdadb1fca9986de034d3b99d1223749a948
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-01-29 12:02:11 +0300

    project setup

---
+++ eureka-server/src/test/java/maksim/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package maksim.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
