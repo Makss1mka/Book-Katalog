6bf1ee268d5da0323e8aa0631540fd88807f9221
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-02-08 14:59:36 +0300

    Add app config (for extracting secret  string from conf file). Add integration with eureka server.

--- auth-service/src/main/java/maksim/auth_service/config/AppConfig.java
+++ auth-service/src/main/java/maksim/auth_service/config/AppConfig.java
@@ -5,9 +5,12 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Getter @Setter
 @Component
 public class AppConfig {
     @Value("${spring.application.secret}")
     private String secret;
+
+    public String getSecret() {
+        return secret;
+    }
 }
