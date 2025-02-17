b86b9aa373526e2f6c21e5b18c33a8ef3f715346
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-02-17 21:29:17 +0300

    Add Global exception handler class for review-service. Add Pagination bean for review-service.

---
+++ review-service/src/main/java/maksim/review_service/utils/Pagination.java
@@ -0,0 +1,34 @@
+package maksim.review_service.utils;
+
+import jakarta.ws.rs.BadRequestException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Pagination {
+
+    public Sort getSort(String sortField, String sortDir) {
+        Sort sort = Sort.by(sortField);
+
+        if(sortDir.equals("asc")) {
+            sort = sort.ascending();
+        } else if (sortDir.equals("desc")) {
+            sort = sort.descending();
+        } else {
+            throw new BadRequestException("Invalid sort direction value");
+        }
+
+        return sort;
+    }
+
+    public Pageable getPageable(int pageNum, int itemsAmount, Sort sort) {
+        return PageRequest.of(pageNum, itemsAmount, sort);
+    }
+
+    public Pageable getPageable(int pageNum, int itemsAmount, String sortField, String sortDir) {
+        return PageRequest.of(pageNum, itemsAmount, getSort(sortField, sortDir));
+    }
+
+}
