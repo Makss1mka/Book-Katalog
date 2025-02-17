748129923b6881bca29f51cd719a93459673e43c
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-02-18 02:07:14 +0300

    Add enums, for more convinient code reading.

---
+++ book-service/src/main/java/maksim/book_service/utils/BookStatus.java
@@ -0,0 +1,25 @@
+package maksim.book_service.utils;
+
+import lombok.Getter;
+
+@Getter
+public enum BookStatus {
+    DROP("drop"),
+    READ("read"),
+    READING("reading");
+
+    private final String value;
+
+    BookStatus(String value) {
+        this.value = value;
+    }
+
+    public static BookStatus fromValue(String value) {
+        for (BookStatus field : values()) {
+            if (field.value.equalsIgnoreCase(value)) {
+                return field;
+            }
+        }
+        throw new IllegalArgumentException("Unknown sort dir field: " + value);
+    }
+}
---
+++ book-service/src/main/java/maksim/book_service/utils/SortField.java
@@ -0,0 +1,42 @@
+package maksim.book_service.utils;
+
+import lombok.Getter;
+
+@Getter
+public enum SortField {
+    RATING("rating"),
+    NAME("name"),
+    AUTHOR("author"),
+    RATINGS_COUNT("ratings count"),
+    ISSUED_DATE("issued_date"),
+
+    STATUS_DROP_LAST_WEEK("status_drop_last_week"),
+    STATUS_DROP_LAST_MONTH("status_drop_last_month"),
+    STATUS_DROP_LAST_YEAR("status_drop_last_year"),
+    STATUS_DROP_OVERALL("status_drop_overall"),
+
+    STATUS_READ_LAST_WEEK("status_read_last_week"),
+    STATUS_READ_LAST_MONTH("status_read_last_month"),
+    STATUS_READ_LAST_YEAR("status_read_last_year"),
+    STATUS_READ_OVERALL("status_read_overall"),
+
+    STATUS_READING_LAST_WEEK("status_reading_last_week"),
+    STATUS_READING_LAST_MONTH("status_reading_last_month"),
+    STATUS_READING_LAST_YEAR("status_reading_last_year"),
+    STATUS_READING_OVERALL("status_reading_overall");
+
+    private final String value;
+
+    SortField(String value) {
+        this.value = value;
+    }
+
+    public static SortField fromValue(String value) {
+        for (SortField field : values()) {
+            if (field.value.equalsIgnoreCase(value)) {
+                return field;
+            }
+        }
+        throw new IllegalArgumentException("Unknown sort field: " + value);
+    }
+}
