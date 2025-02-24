f17d5c55451d5e2d956b260f9bf1399e8bbe1726
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-02-24 11:46:27 +0300

    Rewrite security class for temporally allowing all reqs.

--- review-service/src/main/java/maksim/review_service/config/SecurityConfig.java
+++ review-service/src/main/java/maksim/review_service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,7 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll() // Разрешаем все запросы
+            )
+            .csrf(AbstractHttpConfigurer::disable); // Отключаем CSRF-защиту (если не требуется)
+
         return http.build();
     }
 
