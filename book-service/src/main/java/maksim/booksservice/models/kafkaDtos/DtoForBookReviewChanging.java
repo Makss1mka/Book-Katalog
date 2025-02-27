aef61a3cd97777453ab60ce439d14462ed640303
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-02-27 23:58:16 +0300

    Add kafka integration.

---
+++ book-service/src/main/java/maksim/booksservice/models/kafkaDtos/DtoForBookReviewChanging.java
@@ -0,0 +1,21 @@
+package maksim.booksservice.models.kafkaDtos;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class DtoForBookReviewChanging {
+
+    // -1 - remove rate / 0 - change rate / 1 - add rate
+    private Integer action;
+
+    private Integer bookId;
+
+    private Integer rating;
+
+    private Integer previousRate;
+
+}
