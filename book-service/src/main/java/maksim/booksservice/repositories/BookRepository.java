b30e9d532ea43608dcd18d6fed758507b2f47868
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-03-02 02:30:47 +0300

    Remove all methods. Add 4 methods:  - find by id with join  - find by id  - find all with join, with spec  - find all with spec

--- book-service/src/main/java/maksim/booksservice/repositories/BookRepository.java
+++ book-service/src/main/java/maksim/booksservice/repositories/BookRepository.java
@@ -1,41 +1,18 @@
 package maksim.booksservice.repositories;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 import maksim.booksservice.models.Book;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book> {
+    @EntityGraph(value = "Book.author", type = EntityGraph.EntityGraphType.FETCH)
+    public Optional<Book> findByIdWithJoin(int id);
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
-    public Optional<Book> findById(int id);
-
-    @EntityGraph(attributePaths = {"author"})
-    public List<Book> findByName(String name, Pageable pageable);
-
-    public List<Book> findByRating(int rating, Pageable pageable);
-
-    public List<Book> findByRatingCount(int ratingCount, Pageable pageable);
-
-    @Query("SELECT b FROM Book b JOIN FETCH b.author a WHERE a.name = :authorName")
-    public List<Book> findByAuthorName(@Param("authorName") String authorName, Pageable pageable);
-
-    @Query("SELECT b FROM Book b JOIN FETCH b.author a WHERE a.id = :id")
-    public List<Book> findByAuthorId(@Param("id") int id, Pageable pageable);
-
-    public List<Book> findByRatingGreaterThan(int rating, Pageable pageable);
-
-    public List<Book> findByRatingLessThan(int rating, Pageable pageable);
-
-    public List<Book> findByIssuedDateGreaterThan(Date issuedDate, Pageable pageable);
-
-    public List<Book> findByIssuedDateLessThan(Date issuedDate, Pageable pageable);
-
-    @Query("SELECT b FROM Book b JOIN FETCH b.author a WHERE b.genres IN :genres")
-    public List<Book> findAllByGenres(@Param("genres") List<String> genres, Pageable pageable);
-
+    @EntityGraph(value = "Book.author", type = EntityGraph.EntityGraphType.FETCH)
+    public Page<Book> findByAllWithJoin(Specification<Book> spec, Pageable pageable);
 }
