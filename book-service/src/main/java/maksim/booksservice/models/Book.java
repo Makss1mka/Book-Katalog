5e5794ed8c7c682d5c8f5af84bae4b2a464ab5b3
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-03-02 02:28:56 +0300

    Add entityGraph support. Change fetch type to lazy.

--- book-service/src/main/java/maksim/booksservice/models/Book.java
+++ book-service/src/main/java/maksim/booksservice/models/Book.java
@@ -1,17 +1,8 @@
 package maksim.booksservice.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
+
 import java.util.Date;
 import java.util.List;
 import lombok.Getter;
@@ -23,8 +14,13 @@ import lombok.ToString;
 @ToString
 @Entity
 @Table(name = "books")
+@NamedEntityGraph(name = "Book.withAuthor",
+    attributeNodes = {
+        @NamedAttributeNode("author")
+    }
+)
 public class Book {
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "author_id")
     private User author;
 
