5e03b858e7e924be5dda41690ce68418a9f506f8
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2025-03-03 16:17:10 +0300

    Change find methods

--- book-service/src/main/java/maksim/booksservice/repositories/BookRepository.java
+++ book-service/src/main/java/maksim/booksservice/repositories/BookRepository.java
@@ -2,17 +2,17 @@ package maksim.booksservice.repositories;
 
 import java.util.Optional;
 import maksim.booksservice.models.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book> {
-    @EntityGraph(value = "Book.author", type = EntityGraph.EntityGraphType.FETCH)
-    Optional<Book> findByIdWithJoin(int id);
+    @EntityGraph(attributePaths = {"author"})
+    @Query("SELECT b FROM Book b WHERE b.id = :id")
+    Optional<Book> findByIdWithAuthor(@Param("id") int id);
 
-    @EntityGraph(value = "Book.author", type = EntityGraph.EntityGraphType.FETCH)
-    Page<Book> findAllWithJoin(Specification<Book> spec, Pageable pageable);
+    @Query("SELECT b FROM Book b WHERE b.id = :id")
+    Optional<Book> findByIdWithoutAuthor(@Param("id") int id);
 }
