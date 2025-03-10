741bf3a9ac7e42032f36fadaedddb245f7124713
Makss1mka <maksim_goncharov_2006@mail.ru>
2025-03-10 20:30:36 +0300

    Rewrote logic for deleting adding updating statuses

--- user-service/src/main/java/maksim/user_service/services/UserService.java
+++ user-service/src/main/java/maksim/user_service/services/UserService.java
@@ -2,13 +2,16 @@ package maksim.user_service.services;
 
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.BadRequestException;
-import jakarta.ws.rs.InternalServerErrorException;
 import jakarta.ws.rs.NotFoundException;
 import maksim.user_service.config.AppConfig;
+import maksim.user_service.exceptions.ConflictException;
+import maksim.user_service.exceptions.NoContentException;
 import maksim.user_service.models.Book;
 import maksim.user_service.models.User;
 import maksim.user_service.models.UserBookStatuses;
+import maksim.user_service.models.dtos.CreateBookStatusDto;
 import maksim.user_service.models.dtos.CreateUserDto;
+import maksim.user_service.models.dtos.UpdateBookStatusDto;
 import maksim.user_service.models.dtos.UpdateUserDto;
 import maksim.user_service.repositories.UserRepository;
 import maksim.user_service.utils.enums.BookStatus;
@@ -16,14 +19,12 @@ import maksim.user_service.utils.enums.JoinMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -60,8 +61,6 @@ public class UserService {
         };
 
         if (optionalUser.isEmpty()) {
-            logger.trace("UserService method end: getUserById | User with such id not found");
-
             throw new NotFoundException("Cannot find user such id");
         }
 
@@ -104,42 +103,12 @@ public class UserService {
 
         Optional<User> comparableUser = userRepository.findByEmail(userDto.getEmail());
         if (comparableUser.isPresent()) {
-            logger.trace("UserService method: createUser | Cannot add user: email is taken");
-
-            throw new BadRequestException("Cannot add user. Email is taken.");
+            throw new ConflictException("Cannot add user. Email is taken.");
         }
 
         comparableUser = userRepository.findByName(userDto.getName());
         if (comparableUser.isPresent()) {
-            logger.trace("UserService method: createUser | Cannot add user: name is taken");
-
-            throw new BadRequestException("Cannot add user. Name is taken.");
-        }
-
-        boolean isPasswordContainsDigit = false;
-        boolean isPasswordContainsSpecs = false;
-        boolean isPasswordContainsLetters = false;
-        byte[] chars = userDto.getPassword().getBytes();
-        for (int i = 0; i < userDto.getPassword().length(); i++) {
-            if (!isPasswordContainsDigit && (chars[i] >= 48 && chars[i] <= 57)) {
-                isPasswordContainsDigit = true;
-                continue;
-            }
-
-            if (!isPasswordContainsLetters && ((chars[i] >= 65 && chars[i] <= 90) || (chars[i] >= 97 && chars[i] <= 122))) {
-                isPasswordContainsLetters = true;
-                continue;
-            }
-
-            if (!isPasswordContainsSpecs && (chars[i] >= 33 && chars[i] <= 42)) {
-                isPasswordContainsSpecs = true;
-            }
-        }
-
-        if (!isPasswordContainsDigit || !isPasswordContainsLetters || !isPasswordContainsSpecs) {
-            logger.trace("UserService method: createUser | Cannot add user: name is taken");
-
-            throw new BadRequestException("Password should contains at least: 1 spec symbol, 1 letter, 1 digit.");
+            throw new ConflictException("Cannot add user. Name is taken.");
         }
 
         User newUser = new User();
@@ -149,13 +118,7 @@ public class UserService {
             passwordEncoder.encode(userDto.getPassword())
         );
 
-        try {
-            userRepository.save(newUser);
-        } catch(DataIntegrityViolationException ex) {
-            logger.warn("UserService method exception: createUser | Cannot add user: {}", ex.getMessage());
-
-            throw new InternalServerErrorException("Cannot add user");
-        }
+        userRepository.save(newUser);
 
         logger.trace("UserService method end: createUser | User was found");
 
@@ -163,147 +126,147 @@ public class UserService {
     }
 
     @Transactional
-    public void addStatus(int userId, int bookId, BookStatus status) {
+    public User createStatus(int userId, CreateBookStatusDto statusDto) {
         logger.trace("UserService method entrance: addStatus | Params: user id {} ; book id {} ; status {}",
-                userId, bookId, status);
+                userId, statusDto.getBookId(), statusDto.getStatus());
 
-        // User existence check
-        Optional<User> user = userRepository.findByIdWithJoinStatusesAndBooks(userId);
-        if (user.isEmpty()) {
-            logger.trace("UserService method: addStatus | Cannot find user with such id");
-
-            throw new NotFoundException("User with such id does not exist");
-        }
+        User user = getUserById(userId, JoinMode.WITH_STATUSES_AND_BOOKS);
 
         // Status existence check
-        for (UserBookStatuses st : user.get().getUserBookStatuses()) {
-            if (st.getBook().getId() == bookId) {
-                logger.trace("UserService method: addStatus | Status for such user and book already exist");
-
-                throw new NotFoundException("Status for such user and book already exist");
+        for (UserBookStatuses st : user.getUserBookStatuses()) {
+            if (st.getBook().getId() == statusDto.getBookId()) {
+                throw new ConflictException("Status for such user and book already exist");
             }
         }
 
         // Book existence check
         ResponseEntity<Book> bookRequest = restTemplate.getForEntity(
-                appConfig.getBookServiceUrl() + "/books/" + bookId,
+                appConfig.getBookServiceUrl() + "/books/" + statusDto.getBookId(),
                 Book.class);
         if (bookRequest.getStatusCode() != HttpStatus.OK) {
-            logger.trace("UserService method: addStatus | Status for such user and book already exist");
-
-            throw new BadRequestException("Status for such user and book already exist");
+            throw new BadRequestException("Cannot get book with such id");
         }
 
         // Create status
         UserBookStatuses newStatus = new UserBookStatuses();
         newStatus.setBook(bookRequest.getBody());
-        switch (status) {
+        switch (statusDto.getStatus()) {
             case READ -> newStatus.setStatusRead(true);
             case READING -> newStatus.setStatusReading(true);
             case DROP -> newStatus.setStatusDrop(true);
             case LIKED -> newStatus.setLike(true);
         }
 
-        // Save status
-        user.get().getUserBookStatuses().add(newStatus);
-        userRepository.save(user.get());
+        // Create and save status
+        user.getUserBookStatuses().add(newStatus);
+        userRepository.save(user);
 
         logger.trace("UserService method end: addStatus | Status was added successfully");
+
+        return user;
     }
 
 
+
     @Transactional
-    public void changeStatus(int userId, int bookId, BookStatus status) {
+    public User updateStatus(int userId, UpdateBookStatusDto userDto) {
         logger.trace("UserService method entrance: changeStatus | Params: user id {} ; book id {} ; new status {}",
-                userId, bookId, status);
+                userId, userDto.getBookId(), userDto.getStatus());
 
-        // User existence check
-        Optional<User> user = userRepository.findByIdWithJoinStatusesAndBooks(userId);
-        if (user.isEmpty()) {
-            logger.trace("UserService method: changeStatus | Cannot find user with such id");
-
-            throw new NotFoundException("User with such id does not exist");
-        }
+        User user = getUserById(userId, JoinMode.WITH_STATUSES_AND_BOOKS);
 
         UserBookStatuses statusEntity = null;
 
         // Status existence check
-        for (UserBookStatuses st : user.get().getUserBookStatuses()) {
-            if (st.getBook().getId() == bookId) {
+        for (UserBookStatuses st : user.getUserBookStatuses()) {
+            if (st.getBook().getId() == userDto.getBookId()) {
                 statusEntity = st;
             }
         }
 
         if (statusEntity == null) {
-            logger.trace("UserService method: changeStatus | Cannot find status with such user and book ids");
-
             throw new NotFoundException("Cannot find status with such user and book ids");
         }
 
-        // Status changing
-        switch (status) {
-            case READ -> {
-                if (statusEntity.getStatusRead()) return;
+        boolean isSmthChanged = false;
 
-                statusEntity.setStatusReading(false);
-                statusEntity.setStatusDrop(false);
-                statusEntity.setStatusRead(true);
+        // Status changing
+        switch (userDto.getStatus()) {
+            case READ -> {
+                if (statusEntity.getStatusRead() == userDto.getStatusValue()) {
+                    statusEntity.setStatusReading(false);
+                    statusEntity.setStatusDrop(false);
+                    statusEntity.setStatusRead(userDto.getStatusValue());
+
+                    isSmthChanged = true;
+                }
             }
             case READING -> {
-                if (statusEntity.getStatusReading()) return;
+                if (statusEntity.getStatusReading() != userDto.getStatusValue()) {
+                    statusEntity.setStatusReading(userDto.getStatusValue());
+                    statusEntity.setStatusDrop(false);
+                    statusEntity.setStatusRead(false);
 
-                statusEntity.setStatusReading(true);
-                statusEntity.setStatusDrop(false);
-                statusEntity.setStatusRead(false);
+                    isSmthChanged = true;
+                }
             }
             case DROP -> {
-                if (statusEntity.getStatusDrop()) return;
+                if (statusEntity.getStatusDrop() != userDto.getStatusValue()) {
+                    statusEntity.setStatusReading(false);
+                    statusEntity.setStatusRead(false);
+                    statusEntity.setStatusDrop(userDto.getStatusValue());
 
-                statusEntity.setStatusReading(false);
-                statusEntity.setStatusRead(false);
-                statusEntity.setStatusDrop(true);
+                    isSmthChanged = true;
+                }
             }
-            default -> {
-                logger.trace("UserService method: changeStatus | Invalid status value");
+            case LIKED -> {
+                if (statusEntity.getLike() != userDto.getStatusValue()) {
+                    statusEntity.setLike(userDto.getStatusValue());
 
-                throw new BadRequestException("Invalid status value. Status can be: read, reading, drop");
+                    isSmthChanged = true;
+                }
             }
         }
 
-        userRepository.save(user.get());
+        if (!statusEntity.getStatusReading() && !statusEntity.getStatusRead()
+            && !statusEntity.getStatusDrop() && !statusEntity.getLike()) {
+            deleteStatusEntity(userId, userDto.getBookId());
+        } else {
+            if (isSmthChanged) {
+                userRepository.save(user);
+            } else {
+                throw new NoContentException("Nothing has changed");
+            }
+        }
 
         logger.trace("UserService method end: changeStatus | Status was changed successfully");
+
+        return user;
     }
 
     @Transactional
-    public void changeUser(int userId, UpdateUserDto userDto) {
+    public User updateUser(int userId, UpdateUserDto userDto) {
         logger.trace("UserService method entrance: changeUser");
 
-        // User existence check
-        Optional<User> user = userRepository.findByIdWithJoinStatusesAndBooks(userId);
-        if (user.isEmpty()) {
-            logger.info("UserService method: changeUser | Cannot find user with such id");
-
-            throw new NotFoundException("User with such id does not exist");
-        }
+        User user = getUserById(userId, JoinMode.WITH_STATUSES_AND_BOOKS);
 
         // Change values
         boolean isSmthChanged = false;
 
-        if (userDto.getNewName() != null && !userDto.getNewName().equals(user.get().getName())) {
-            user.get().setName(userDto.getNewName());
+        if (userDto.getNewName() != null && !userDto.getNewName().equals(user.getName())) {
+            user.setName(userDto.getNewName());
             isSmthChanged = true;
         }
 
-        if (userDto.getNewEmail() != null && !userDto.getNewEmail().equals(user.get().getEmail())) {
-            user.get().setEmail(userDto.getNewEmail());
+        if (userDto.getNewEmail() != null && !userDto.getNewEmail().equals(user.getEmail())) {
+            user.setEmail(userDto.getNewEmail());
             isSmthChanged = true;
         }
 
         if (userDto.getNewPassword() != null && userDto.getOldPassword() != null) {
             if (passwordEncoder.matches(
                 userDto.getOldPassword(),
-                user.get().getPassword()
+                user.getPassword()
             )) {
                 if (Objects.equals(userDto.getOldPassword(), userDto.getNewPassword())) {
                     logger.info("UserService method: changeUser | Old password and new password are equal");
@@ -325,65 +288,55 @@ public class UserService {
             }
         }
 
-        // Saving date
-        if (isSmthChanged) userRepository.save(user.get());
+        // Saving data
+        if (isSmthChanged) userRepository.save(user);
 
         logger.trace("UserService method end: changeUser | Status was changed successfully");
+
+        return user;
     }
 
 
+
     @Transactional
-    public void deleteStatus(int userId, int bookId) {
+    public User deleteStatusEntity(int userId, int bookId) {
         logger.trace("UserService method entrance: deleteStatus | Params: user id {} ; book id {}", userId, bookId);
 
-        // User existence check
-        Optional<User> user = userRepository.findByIdWithJoinStatusesAndBooks(userId);
-        if (user.isEmpty()) {
-            logger.trace("UserService method: deleteStatus | Cannot find user with such id");
+        User user = getUserById(userId, JoinMode.WITH_STATUSES_AND_BOOKS);
 
-            throw new NotFoundException("User with such id does not exist");
-        }
-
-        List<UserBookStatuses> statuses = user.get().getUserBookStatuses();
+        List<UserBookStatuses> statuses = user.getUserBookStatuses();
         boolean isFound = false;
 
         // Status existence check
         for (int i = 0; i < statuses.size(); i++) {
             if (statuses.get(i).getBook().getId() == bookId) {
                 statuses.remove(i);
+
                 isFound = true;
                 break;
             }
         }
 
         if (!isFound) {
-            logger.trace("UserService method: deleteStatus | Cannot find status for that book in user's statuses");
-
             throw new NotFoundException("Cannot find status for that book in user's statuses");
         }
 
-        userRepository.save(user.get());
+        userRepository.save(user);
 
         logger.trace("UserService method end: deleteStatus | Status was changed successfully");
+
+        return user;
     }
 
     @Transactional
     public void deleteUser(int userId) {
         logger.trace("UserService method entrance: deleteUser | Params: user id {}", userId);
 
-        // User existence check
-        Optional<User> user = userRepository.findByIdWithJoinStatusesAndBooks(userId);
-        if (user.isEmpty()) {
-            logger.trace("UserService method: deleteUser | Cannot find user with such id");
+        User user = getUserById(userId, JoinMode.WITH_STATUSES_AND_BOOKS);
 
-            throw new NotFoundException("User with such id does not exist");
-        }
-
-        userRepository.delete(user.get());
+        userRepository.delete(user);
 
         logger.trace("UserService method end: deleteUser | User was deleted successfully");
     }
-
-
 
 }
