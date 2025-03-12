033b763e27d1b937aac4da54df09eedf2793ec3f
maksim <maksim_goncharov_2006@mail.ru>
2025-03-12 13:08:27 +0300

    Remove some unnecessary fields and annotations

---
+++ book-service/src/main/java/maksim/booksservice/models/entities/Book.java
@@ -0,0 +1,52 @@
+package maksim.booksservice.models.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "books")
+public class Book {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author = null;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "book_id")
+    private List<BookStatusLog> statusesLogs = null;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "rating", nullable = false)
+    private float rating = 0;
+
+    @Column(name = "ratings_count", nullable = false)
+    private int ratingCount = 0;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "issued_date", nullable = false)
+    private Date issuedDate;
+
+    @Column(name = "genres")
+    private List<String> genres = new ArrayList<>();
+
+    @Column(name = "likes", nullable = false)
+    private int likes = 0;
+}
