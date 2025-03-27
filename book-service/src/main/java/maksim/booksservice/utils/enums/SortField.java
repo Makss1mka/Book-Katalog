3e3726cb12cc7d06ed1542f834daecb18a4995d7
Максим <maksim_goncharov_2006@mail.ru>
2025-03-27 20:50:31 +0300

    Remove invalid fields

--- book-service/src/main/java/maksim/booksservice/utils/enums/SortField.java
+++ book-service/src/main/java/maksim/booksservice/utils/enums/SortField.java
@@ -6,24 +6,8 @@ import lombok.Getter;
 public enum SortField {
     RATING("rating"),
     NAME("name"),
-    AUTHOR("author"),
-    RATINGS_COUNT("ratings count"),
-    ISSUED_DATE("issued_date"),
-
-    STATUS_DROP_LAST_WEEK("status_drop_last_week"),
-    STATUS_DROP_LAST_MONTH("status_drop_last_month"),
-    STATUS_DROP_LAST_YEAR("status_drop_last_year"),
-    STATUS_DROP_OVERALL("status_drop_overall"),
-
-    STATUS_READ_LAST_WEEK("status_read_last_week"),
-    STATUS_READ_LAST_MONTH("status_read_last_month"),
-    STATUS_READ_LAST_YEAR("status_read_last_year"),
-    STATUS_READ_OVERALL("status_read_overall"),
-
-    STATUS_READING_LAST_WEEK("status_reading_last_week"),
-    STATUS_READING_LAST_MONTH("status_reading_last_month"),
-    STATUS_READING_LAST_YEAR("status_reading_last_year"),
-    STATUS_READING_OVERALL("status_reading_overall");
+    RATINGS_COUNT("ratings-count"),
+    ISSUED_DATE("issued-date");
 
     private final String value;
 
