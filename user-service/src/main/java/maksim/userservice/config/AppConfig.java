dca66cbb41c7218d73c696e03ebdaa286dda5f4e
Максим <maksim_goncharov_2006@mail.ru>
2025-03-28 12:37:10 +0300

    User service appConfig fix

--- user-service/src/main/java/maksim/userservice/config/AppConfig.java
+++ user-service/src/main/java/maksim/userservice/config/AppConfig.java
@@ -1,16 +1,18 @@
 package maksim.userservice.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
+@Setter
 public class AppConfig {
     @Value("${spring.application.users-profiles-directory}")
     private String bookFilesDirectory;
 
-    @Value("{spring.services.book-service-url}")
+    @Value("${spring.services.book-service-url}")
     private String bookServiceUrl;
 
 }
