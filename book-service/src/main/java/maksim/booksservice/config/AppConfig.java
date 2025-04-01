5f7e4405ef328b4a5b833dfa63ac9dfe3f30a6f7
Максим <maksim_goncharov_2006@mail.ru>
2025-04-01 03:05:14 +0300

    Add restTemplate config to book service, add url for user-service in bokk service, add in app config user-service url in book service

--- book-service/src/main/java/maksim/booksservice/config/AppConfig.java
+++ book-service/src/main/java/maksim/booksservice/config/AppConfig.java
@@ -1,14 +1,15 @@
 package maksim.booksservice.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 public class AppConfig {
     @Value("${spring.application.book-files-directory}")
     private String bookFilesDirectory;
 
-    public String getBookFilesDirectory() {
-        return bookFilesDirectory;
-    }
+    @Value("${spring.services.user-service-url}")
+    private String userServiceUrl;
 }
