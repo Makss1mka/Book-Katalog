c14610ac2077506ecbea548e87ad7669d7a4efd1
Максим <maksim_goncharov_2006@mail.ru>
2025-04-10 21:20:10 +0300

    Add values for kafka

--- book-service/src/main/java/maksim/booksservice/config/AppConfig.java
+++ book-service/src/main/java/maksim/booksservice/config/AppConfig.java
@@ -7,9 +7,40 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class AppConfig {
+    private final String kafkaPath = "spring.kafka.topics.";
+
+
     @Value("${spring.application.book-files-directory}")
     private String bookFilesDirectory;
 
+
+    /*
+        SERViCES PARAMS:
+        urls
+     */
+
     @Value("${spring.services.user-service-url}")
     private String userServiceUrl;
+
+
+    /*
+        KAFKA PARAMETERS:
+        topic's names
+     */
+
+    @Value("${" + kafkaPath + "status-update" + "}")
+    private String kafkaStatusUpdateTopic;
+
+    @Value("${" + kafkaPath + "status-create" + "}")
+    private String kafkaStatusCreateTopic;
+
+    @Value("${" + kafkaPath + "status-delete" + "}")
+    private String kafkaStatusDeleteTopic;
+
+    @Value("${" + kafkaPath + "like-create" + "}")
+    private String kafkaLikeCreateTopic;
+
+    @Value("${" + kafkaPath + "like-delete" + "}")
+    private String kafkaLikeDeleteTopic;
+
 }
--- user-service/src/main/java/maksim/userservice/config/AppConfig.java
+++ user-service/src/main/java/maksim/userservice/config/AppConfig.java
@@ -9,10 +9,39 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 public class AppConfig {
+    private final String kafkaPath = "spring.kafka.topics.";
+
+
+    /*
+        SERViCES PARAMS:
+        urls
+     */
+
     @Value("${spring.application.users-profiles-directory}")
     private String bookFilesDirectory;
 
     @Value("${spring.services.book-service-url}")
     private String bookServiceUrl;
+
+
+    /*
+        KAFKA PARAMETERS:
+        topic's names
+     */
+
+    @Value("${" + kafkaPath + "status-update" + "}")
+    private String kafkaStatusUpdateTopic;
+
+    @Value("${" + kafkaPath + "status-create" + "}")
+    private String kafkaStatusCreateTopic;
+
+    @Value("${" + kafkaPath + "status-delete" + "}")
+    private String kafkaStatusDeleteTopic;
+
+    @Value("${" + kafkaPath + "like-create" + "}")
+    private String kafkaLikeCreateTopic;
+
+    @Value("${" + kafkaPath + "like-delete" + "}")
+    private String kafkaLikeDeleteTopic;
 
 }
