f52422e09f728882493ad153d3d588da4c45c047
Максим <maksim_goncharov_2006@mail.ru>
2025-04-12 18:14:45 +0300

    Add async config in book service

---
+++ book-service/src/main/java/maksim/booksservice/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package maksim.booksservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "logsTaskExecutor")
+    public Executor logsTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("LogsThread-");
+        executor.initialize();
+        return executor;
+    }
+
+}
