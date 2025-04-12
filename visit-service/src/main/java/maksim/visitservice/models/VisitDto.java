710a7c0c13c069862a951af1cdc8d419650022a5
Максим <maksim_goncharov_2006@mail.ru>
2025-04-12 18:11:25 +0300

    Change field name url -> serviceName

--- visit-service/src/main/java/maksim/visitservice/models/VisitDto.java
+++ visit-service/src/main/java/maksim/visitservice/models/VisitDto.java
@@ -9,14 +9,14 @@ import lombok.ToString;
 @ToString
 public class VisitDto {
 
-    private String url;
+    private String serviceName;
 
     private String method;
 
     private Long count;
 
     public VisitDto(Visit visit) {
-        this.url = visit.getUrl();
+        this.serviceName = visit.getServiceName();
         this.method = visit.getMethod();
         this.count = visit.getCount();
     }
