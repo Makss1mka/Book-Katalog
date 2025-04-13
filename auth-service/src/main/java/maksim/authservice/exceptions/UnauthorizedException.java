528a6d68bc4d0211faca3eee86e8a9adca86177c
Максим <maksim_goncharov_2006@mail.ru>
2025-04-14 00:41:25 +0300

    Add exception classes to auth service

---
+++ auth-service/src/main/java/maksim/authservice/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package maksim.authservice.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
